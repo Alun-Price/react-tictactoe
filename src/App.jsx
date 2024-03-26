691b00934c2cd180127c4533a436e48d03003abe
Alun Price <alunprice089@gmail.com>
2024-03-26 02:20:17 +0000

    Added Board and Square components; click to get an  on an empty board square

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,44 @@
-export default function Square()
+import { useState } from 'react';
+
+function Square()
 {
-  return <button className="square">X</button>;
+  const [ value, setValue ] = useState( "" );
+
+  function handleClick()
+  {
+    setValue( "X" );
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={ handleClick }
+    >
+      { value }
+    </button>
+  );
+}
+
+
+export default function Board()
+{
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }

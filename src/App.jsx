a85e9258daab69043ee40385074e9c9a6258efa4
Alun Price <alunprice089@gmail.com>
2024-03-27 13:31:23 +0000

    Added winner calculation and coloured icons; working tic-tac-toe game at this point

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,18 @@ import { useState } from 'react';
 
 function Square( { value, onSquareClick } )
 {
+
+  let iconColor;
+
+  if ( value == 'X' )
+  {
+    iconColor = "purple";
+  } else
+  {
+    iconColor = "green";
+  }
   return (
-    <button className="square" onClick={ onSquareClick }>
+    <button className="square" style={ { "color": iconColor } } onClick={ onSquareClick }>
       { value }
     </button>
   );
@@ -18,7 +28,7 @@ export default function Board()
 
   function handleClick( i )
   {
-    if ( squares[ i ] )
+    if ( squares[ i ] || calculateWinner( squares ) )
     {
       return;
     }
@@ -34,8 +44,20 @@ export default function Board()
     setXIsNext( !xIsNext );
   }
 
+  const winner = calculateWinner( squares );
+  let status;
+  if ( winner )
+  {
+    status = "Winner: " + winner;
+  } else
+  {
+    status = "Next player: " + ( xIsNext ? "X" : "O" );
+  }
+
   return (
     <>
+      <div className='status'>{ status }</div>
+      <br />
       <div className="board-row">
         <Square value={ squares[ 0 ] } onSquareClick={ () => handleClick( 0 ) } />
         <Square value={ squares[ 1 ] } onSquareClick={ () => handleClick( 1 ) } />
@@ -53,4 +75,27 @@ export default function Board()
       </div>
     </>
   );
+}
+
+function calculateWinner( squares )
+{
+  const lines = [
+    [ 0, 1, 2 ],
+    [ 3, 4, 5 ],
+    [ 6, 7, 8 ],
+    [ 0, 3, 6 ],
+    [ 1, 4, 7 ],
+    [ 2, 5, 8 ],
+    [ 0, 4, 8 ],
+    [ 2, 4, 6 ]
+  ];
+  for ( let i = 0; i < lines.length; i++ )
+  {
+    const [ a, b, c ] = lines[ i ];
+    if ( squares[ a ] && squares[ a ] === squares[ b ] && squares[ a ] === squares[ c ] )
+    {
+      return squares[ a ];
+    }
+  }
+  return null;
 }
